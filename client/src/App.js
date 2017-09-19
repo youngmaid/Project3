132d5005942b61f742ead3b2ee3f7bb650f898e9
Natasha Umer <natasha.umer2@gmail.com>
2017-09-19 15:19:16 -0400

    added initial react skeleton

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Route, Redirect, Switch} from 'react-router-dom';
+
+
+import Home from './components/Home';
+import FaceList from './components/QuoteList'
+import FaceForm from './components/FaceForm'
+import About from './components/About'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="quotes">
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/About' component={About} />
+            <Route exact path="/bishfaces" component={FaceList} />
+            <Route exact path="/Add" component={FaceForm} />
+            <Redirect to='/' />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
