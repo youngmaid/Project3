fd411a7b0f41a9d035e9a3b1ccf4ca6d994b8f7c
Zarrina Niyazova <zarrina_@hotmail.com>
2017-09-25 18:00:58 -0400

    css added

--- client/src/components/partials/Loading.js
+++ client/src/components/partials/Loading.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <p>Loading...</p>
+      <div className="bgcolor">
+       <p>Loading...</p>
+      </div>
     );
   };
 }
