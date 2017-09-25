c346824c22ae46153ec9b61518355e5adf42cf45
Zarrina Niyazova <zarrina_@hotmail.com>
2017-09-25 16:56:09 -0400

    Richard css changes

--- src/components/partials/Loading.js
+++ src/components/partials/Loading.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
+      <div className="bgcolor">
       <p>Loading...</p>
+      </div>
     );
   };
 }
