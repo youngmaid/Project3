3f8b9fd4a6f3ea92feae8f24fd05d45576157fc9
Natasha Umer <natasha.umer2@gmail.com>
2017-09-25 14:57:30 -0400

    Revert "Authorization added, servers unified"

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { authRouter, AuthService } = require('./auth');
-const tokenService = require('./auth/TokenService');
 
 const app = express();
 
@@ -14,33 +12,34 @@ app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
 
-app.set('superSecret', process.env.SERVER_SECRET);
-
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(tokenService.receiveToken);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-//ROUTE HANDLER
-const scoresRouter = require('./routes/scores');
-app.use('/auth', authRouter);
-app.use('/api/scores', AuthService.allow({roles: []}), scoresRouter);
+// ROUTE HANDLER
+// app.use('/auth', authRouter);
+// app.use('/api', AuthService.isAuth(), ScoresRouter);
 
-//const scoresRouter = require('./routes/scores');
-//app.use('/api/scores', scoresRouter);
+const scoresRouter = require('./routes/scores');
+app.use('/api/scores', scoresRouter);
 
 app.get('*', function(req, res) {
   res.status(404).send({message: 'Hmm...Not Found.'});
 });
 
+
+
+
+
 module.exports = app;
+
 
 
 
